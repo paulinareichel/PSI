1e11ffa6b276ac4492dba8b7ddcf3960e94fa1db
paulinareichel <paulinareichel@o2.pl>
2018-10-25 23:37:25 +0200

    Update Perceptron.m

---
+++ Perceptron.m
@@ -0,0 +1,11 @@
+%%
+close all; clear all; clc;
+
+net=newp([0 1; -2  2],1);
+x = [0 0 1 1; 0 1 0 1];
+t = [0 0 0 1];
+net = init(net);
+before=sim(net,x)
+net.trainParam.epochs=15;
+net = train(net,x,t);
+after=sim(net,x)
